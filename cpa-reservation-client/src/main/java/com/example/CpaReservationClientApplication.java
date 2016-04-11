de1f49bf118671fe9b2d30455edc0a813b49ea4c
dwong-pivotal <derrick.chwong@gmail.com>
2016-04-11 15:49:14 +0800

    first time work for 1.3.3

--- cpa-reservation-client/src/main/java/com/example/CpaReservationClientApplication.java
+++ cpa-reservation-client/src/main/java/com/example/CpaReservationClientApplication.java
@@ -6,10 +6,12 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.Resources;
@@ -24,6 +26,7 @@ import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -37,22 +40,36 @@ public class CpaReservationClientApplication {
 
 @RequestMapping("/reservations")
 @RestController
+@Configuration
 class ReservationController{
 
-	@Autowired
-	RestTemplate template;
+//	@LoadBalanced @Bean
+//	RestTemplate template = new RestTemplate();
+	
+	@LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+	
+	public Collection<String> getNamesFallback(){
+		return Arrays.asList("Service not available");
+	}
+	
 
+	@HystrixCommand(fallbackMethod="getNamesFallback")
 	@RequestMapping("/names")
 	public Collection<String> getNames(){
-
+		
+		RestTemplate template = restTemplate(); 
 		
 		template.setMessageConverters(getHttpMessageConverters());
 
 		ParameterizedTypeReference<Resources<Reservation>> ptr = new ParameterizedTypeReference<Resources<Reservation>>(){};
-
+		
 		ResponseEntity<Resources<Reservation>> response = 
 				template.exchange("http://cpa-reservation-service/reservations", HttpMethod.GET, null, ptr);
-
+				
 		return response.getBody().getContent()
 				.stream()
 				.map(Reservation::getName)
